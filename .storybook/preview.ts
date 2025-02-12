9c826270288000e7a1288407664d2ebeb787d2dd
dosimpact <ypd03008@gmail.com>
2025-02-13 00:41:02 +0900

    ag grid

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,10 @@
 import type { Preview } from "@storybook/react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
